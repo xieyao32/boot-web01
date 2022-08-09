3d196cbe003197362286d21be554903ffae5508b
xieyao32 <97149367+xieyao32@users.noreply.github.com>
2022-08-09 19:54:57 +0800

    pull commit

--- src/test/java/com/xy/boot/BootWeb01ApplicationTests.java
+++ src/test/java/com/xy/boot/BootWeb01ApplicationTests.java
@@ -14,6 +14,7 @@ class BootWeb01ApplicationTests {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 
 }
